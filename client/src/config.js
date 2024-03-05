09cc46f4f60eaf41ff2ac88331b622d861a70133
Himanshu Sahu <75620224+himanshuusahu@users.noreply.github.com>
2024-03-05 20:20:43 +0530

    Delete client directory

--- client/src/config.js
+++
@@ -1,6 +0,0 @@
-let BASE_URL = "https://backend-devthread.onrender.com";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
-}
-
-export { BASE_URL };
